9a526ac52bb5ad968bb46a6802e2866e85ffdc70
j.majurel <jeanfelix.majurel@gmail.com>
2019-01-09 20:52:04 +0000

    commit before giving back computer

--- src/Contact.js
+++ src/Contact.js
@@ -26,27 +26,29 @@ class Contact extends Component {
 
   render() {
     return (
-      <div className='Page Contact'>
-        <h3>Contactez moi</h3>
-	<form onSubmit={this.handleSubmit}>
-	  <div className='form-group'>
-	    <label htmlFor='name'>Name</label>
-	    <input className='form-control' onChange={this.handleUpdate} type='text' id='name' name='name' placeholder='name' required />
-	  </div>
-	  <div className='form-group'>
-	    <label htmlFor='email'>Email</label>
-	    <input className='form-control' onChange={this.handleUpdate} type='email' id='email' name='email' placeholder='email' required />
-	  </div>
-	  <div className='form-group'>
-	    <label htmlFor='phone'>Phone</label>
-	    <input className='form-control' onChange={this.handleUpdate} type='text' id='phone' name='phone' placeholder='phone number' />
-	  </div>
-	  <div className='form-group'>
-	    <label htmlFor='message'>Message</label>
-	    <textarea className='form-control' onChange={this.handleUpdate} id='message' name='message' rows='8'></textarea>
-	  </div>
-	  <button type='submit' className='btn btn-primary'>Send</button>
-	</form>
+      <div className='Page'>
+	<div className='Contact'>
+	  <h3>Contactez moi</h3>
+	  <form onSubmit={this.handleSubmit}>
+	    <div className='form-group'>
+	      <label htmlFor='name'>Name</label>
+	      <input className='form-control' onChange={this.handleUpdate} type='text' id='name' name='name' placeholder='name' required />
+	    </div>
+	    <div className='form-group'>
+	      <label htmlFor='email'>Email</label>
+	      <input className='form-control' onChange={this.handleUpdate} type='email' id='email' name='email' placeholder='email' required />
+	    </div>
+	    <div className='form-group'>
+	      <label htmlFor='phone'>Phone</label>
+	      <input className='form-control' onChange={this.handleUpdate} type='text' id='phone' name='phone' placeholder='phone number' />
+	    </div>
+	    <div className='form-group'>
+	      <label htmlFor='message'>Message</label>
+	      <textarea className='form-control' onChange={this.handleUpdate} id='message' name='message' rows='8'></textarea>
+	    </div>
+	    <button type='submit' className='btn btn-primary'>Send</button>
+	  </form>
+	</div>
       </div>
     );
   }
--- src/Creations.js
+++ src/Creations.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './Creations.css';
 
 const Creations = props => ((
-  <div className='Page Creations'>
-    <h2>Mes creations</h2>
-    <div className='galery'>
-      <div className='item'>Crea 1</div>
-      <div className='item'>Crea 2</div>
-      <div className='item'>Crea 3</div>
+  <div className='Page'>
+    <div className='Creations'>
+      <h2>Mes creations</h2>
+      <div className='galery'>
+	<div className='item'>Crea 1</div>
+	<div className='item'>Crea 2</div>
+	<div className='item'>Crea 3</div>
+      </div>
     </div>
   </div>
 ));
